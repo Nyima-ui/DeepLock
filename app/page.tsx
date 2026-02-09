59068cff92e775a4435e93ee03e6a84cc5539ed5
Tenzin Nyima <ntenzin492@gmail.com>
2026-02-09 12:46:17 +0530

    take frontend inputs

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,126 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [password, setPassword] = useState("");
+  const PASSWORD_LENGTH = 10;
+
+  function generatePassword() {
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+    const symbols = "!@#$%&*()_+-=[]{|;<>?";
+
+    const allChars = lowercase + uppercase + numbers + symbols;
+
+    let newPassword = "";
+
+    newPassword += lowercase[Math.floor(Math.random() * lowercase.length)];
+    newPassword += uppercase[Math.floor(Math.random() * uppercase.length)];
+    newPassword += numbers[Math.floor(Math.random() * numbers.length)];
+    newPassword += symbols[Math.floor(Math.random() * symbols.length)];
+
+    for (let i = 4; i < PASSWORD_LENGTH; i++) {
+      newPassword += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+
+    newPassword = newPassword
+      .split("")
+      .sort(() => Math.random() - 0.5)
+      .join("");
+    setPassword(newPassword);
+  }
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const date = formData.get("date");
+    const hour = formData.get("hour");
+    const minute = formData.get("minute")
+    console.log(date, hour, minute);
+    generatePassword();
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <div className="p-5">
+      <h1 className="text-5xl">Fill in the details</h1>
+      <form className="mt-5" onSubmit={handleSubmit}>
+        <div className="flex gap-10">
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              placeholder="click Generate"
+              value={password}
+              readOnly
+              className="border block outline-none focus:border-sky-300 rounded-sm py-1 px-1.5 mt-2"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
+
+          <div>
+            <div>
+              <label htmlFor="date">Date</label>
+              <input
+                type="date"
+                className="border block outline-none focus:border-sky-300 rounded-sm py-1 px-1.5 mt-2 cursor-pointer scheme-dark"
+                // defaultValue={new Date().toISOString().split("T")[0]}
+                id="date"
+                name="date"
+                required
+              />
+            </div>
+            <div className="mt-5">
+              <p>Time</p>
+              <div className="flex gap-5">
+                <div>
+                  <label htmlFor="hour">Hour</label>
+                  <select
+                    name="hour"
+                    id="hour"
+                    className="bg-background  ml-3 w-15 cursor-pointer border py-0.5 rounded-sm focus:border-sky-300"
+                    required
+                  >
+                    {Array.from({ length: 24 }).map((_, i) => {
+                      const value = String(i).padStart(2, "0");
+                      return (
+                        <option key={value} value={value}>
+                          {value}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor="hour">Minute</label>
+                  <select
+                    name="minute"
+                    id="minute"
+                    className="bg-background  ml-3 w-15 cursor-pointer border py-0.5 rounded-sm focus:border-sky-300"
+                    required
+                  >
+                    {Array.from({ length: 60 }).map((_, i) => {
+                      const value = String(i).padStart(2, "0");
+                      return (
+                        <option key={value} value={value}>
+                          {value}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
+        <button
+          type="submit"
+          className="cursor-pointer border rounded-sm mt-4 px-4 py-2 hover:border-sky-300"
+        >
+          Generate
+        </button>
+      </form>
     </div>
   );
 }
